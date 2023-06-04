c3a464771129c4fa6ddfd2dd4fa8cf8e0be327a4
1eecan <busyx2modernsociety@gmail.com>
2023-06-04 22:20:33 +0900

    조회 날짜를 현재 날짜로 초기화

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 //https://www.data.go.kr/iim/api/selectAPIAcountView.do 여기서 데이터를 받아오자!
 function weather(nx, ny) {
+  let today = new Date();
+  let year = today.getFullYear();
+  let month = ("0" + (today.getMonth() + 1)).slice(-2);
+  let day = ("0" + today.getDate()).slice(-2);
+  let dateString = year + month + day;
+
   fetch(
-    `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${config.apikey}&pageNo=1&numOfRows=229&dataType=JSON&base_date=20230604&base_time=0500&nx=${nx}&ny=${ny}`
+    `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${config.apikey}&pageNo=1&numOfRows=229&dataType=JSON&base_date=${dateString}&base_time=0500&nx=${nx}&ny=${ny}`
   )
     .then((res) => res.json())
     .then(function (data) {
