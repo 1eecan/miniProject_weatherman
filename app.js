9b27f0e52e4c254b4d3aff0d9e1a29f8a60d1903
1eecan <busyx2modernsociety@gmail.com>
2023-06-04 20:46:38 +0900

    도시목록추가

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //https://www.data.go.kr/iim/api/selectAPIAcountView.do 여기서 데이터를 받아오자!
 
 fetch(
-  `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${apikey}&pageNo=1&numOfRows=229&dataType=JSON&base_date=20230604&base_time=0500&nx=55&ny=127`
+  `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${config.apikey}&pageNo=1&numOfRows=229&dataType=JSON&base_date=20230604&base_time=0500&nx=62&ny=114`
 )
   .then((res) => res.json())
   .then(function (data) {
