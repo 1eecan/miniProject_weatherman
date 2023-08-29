13260e979e35549ab6936c55895a943dd87dfe09
1eecan <busyx2modernsociety@gmail.com>
2023-08-29 14:16:27 +0900

    ✨ 도시 세부 정보(구/군 등등...) 고를 수 있는 기능 추가

--- src/component/CitySelect.js
+++ src/component/CitySelect.js
@@ -1,3 +1,6 @@
+import { locationInformation } from "../core/locationInformation.js";
+import { cityInformation } from "../core/cityInformation.js";
+
 export default function CitySelect({ $target, initialState }) {
   const $citySelect = document.createElement("div");
 
@@ -14,8 +17,46 @@ export default function CitySelect({ $target, initialState }) {
     console.log(this.state);
     const { city, town } = this.state.city;
     $citySelect.innerHTML = `
+            <select class="selectCity">
+            ${cityInformation.map((city, index) =>
+              index === 0
+                ? `<option selected>${city}</option>`
+                : `<option>${city}</option>`
+            )}
+            </select>
+            <select class="selectTown">
+            ${[
+              locationInformation
+                .filter((item) => item[0] === city)
+                .map((town, index) => {
+                  return index === 0
+                    ? `<option selected>${town[1]}</option>`
+                    : `<option>${town[1]}</option>`;
+                }),
+            ]}
+            </select>
             <h2>${city} ${town}의 날씨는...</h2>
         `;
+    const selectedCity = document.querySelector(".selectCity");
+    const selectedTown = document.querySelector(".selectTown");
+    selectedCity.addEventListener("change", () => {
+      selectedTown.length = 0;
+      selectedTown.innerHTML = `
+      ${[
+        locationInformation
+          .filter(
+            (item) =>
+              item[0] === selectedCity.options[selectedCity.selectedIndex].text
+          )
+          .map((town, index) => {
+            return index === 0
+              ? `<option selected>${town[1]}</option>`
+              : `<option>${town[1]}</option>`;
+          }),
+      ]}
+      `;
+      console.log(selectedCity.options[selectedCity.selectedIndex].text);
+    });
   };
 
   this.render();
